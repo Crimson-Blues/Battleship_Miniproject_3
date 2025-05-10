ef0209da2ff1c825d0b0cdf6f719a6975f4a4f11
LisNat <162755027+LisNat@users.noreply.github.com>
2025-05-10 16:40:08 -0500

    Junte lo mío con lo que hizo Manu (nueva lógica)

--- src/main/java/org/example/miniproyecto_3/Model/Ship.java
+++ src/main/java/org/example/miniproyecto_3/Model/Ship.java
@@ -1,21 +1,50 @@
 package org.example.miniproyecto_3.Model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Ship extends ShipAdapter {
-    private int size;
-    private ArrayList<ShipPart> shipParts;
+    private final int length;
+    private ArrayList<Coordinate>positions;
+    private Set<Coordinate> hits;
 
-    public Ship(int size){
-        super();
-        this.size = size;
-        this.shipParts = new ArrayList<>();
-        for(int i = 0; i < size; i++){
-            shipParts.add(new ShipPart(this));
-        }
+    public Ship(int length){
+        if(length<= 0)
+            throw new IllegalArgumentException("Error! La longitud del barco tiene que ser positiva");
+        this.length = length;
+        this.hits = new HashSet<Coordinate>();
     }
 
-    public int getSize() {
-        return size;
+    public int getLength(){
+        return length;
+    }
+
+    public void setCoodinate(ArrayList<Coordinate> pos){
+        if (pos == null || pos.size() != length)
+            throw new IllegalArgumentException("Error! Ingrese una lista de coordenadas correcta");
+        positions = pos;
+    }
+
+    public ArrayList<Coordinate>getPositions(){
+        return positions;
+    }
+    public Set<Coordinate>getHits(){
+        return hits;
+    }
+
+    public boolean contains(Coordinate coor){
+        return positions.contains(coor);
+    }
+
+    public boolean isSunk(){
+        return hits.size() == length;
+    }
+
+    public void hit(Coordinate h){
+        if(!contains(h)){
+            throw new IllegalArgumentException("Error! No puedes hundir una posicion que no le pertenece al barco");
+        }
+        hits.add(h);
     }
 }
